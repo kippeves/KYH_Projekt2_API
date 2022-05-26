3b770441f1b7ec7e61ff840703bc201267f301b7
Kristian Väisänen <kippeves@gmail.com>
2022-05-26 15:22:22 +0200

    Added migration-files that were forgotten in the last commit.

---
+++ KYHProjekt2API/Migrations/20220526070411_Updated Requirements 5.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KYHProjekt2API.Migrations
+{
+    public partial class UpdatedRequirements5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "TimeRegistrations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Projects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Customers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "TimeRegistrations");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Customers");
+        }
+    }
+}
